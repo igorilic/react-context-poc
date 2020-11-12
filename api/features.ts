375dfa109f697b6489d8882ebd6e24176e2b70fd
Igor Ilić <ilicigor@outlook.com>
2020-11-12 16:54:45 +0100

    updated var name

--- api/features.ts
+++ api/features.ts
@@ -1,21 +1,20 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
-const response = {
-  name: "Decrement",
-  description: "Decrement enabler",
-  tags: ["decrement"],
-  constraints: [
-    {
-      dataScope: '{"countryId": "AU"}',
-      enabled: true
-    }
-  ]
-};
-
 export default (request: NowRequest, response: NowResponse) => {
+  const res = {
+    name: "Decrement",
+    description: "Decrement enabler",
+    tags: ["decrement"],
+    constraints: [
+      {
+        dataScope: '{"countryId": "AU"}',
+        enabled: true
+      }
+    ]
+  };
   const { tag = "decrement" } = request.query;
   if (tag === "decrement") {
-    response.status(200).send(response);
+    response.status(200).send(res);
   } else {
     response.status(400).send('tag must be "decrement"');
   }
