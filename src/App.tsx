bc765223ff79676b61cfe8da08b37aa2b3c8e1ef
Igor Ilić <ilicigor@outlook.com>
2020-11-12 15:27:24 +0100

    added counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
-import "./styles.css";
+import {
+  CountProvider,
+  useCountDispatch,
+  useCountState
+} from "./FeatureToggle";
+
+const CountDisplay = () => {
+  const count = useCountState();
+  return <div>{`The current count is ${count.count}`}</div>;
+};
+
+const Counter = () => {
+  const dispatch = useCountDispatch();
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: "increment" })}>+ 1</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>- 1</button>
+    </div>
+  );
+};
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <CountProvider>
+      <CountDisplay />
+      <Counter />
+    </CountProvider>
   );
 }
